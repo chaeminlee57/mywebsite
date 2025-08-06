2c03f6f9ba8e96aeff59033667018e453dc1e9be
Chaemin Lee <chaeminlee57@gmail.com>
2025-08-06 02:04:54 -1000

    more styles and fade in animation

--- main.js
+++ main.js
@@ -13,7 +13,17 @@ function typeWriter(text, elementId, speed = 100, startDelay = 0) {
     setTimeout(type, startDelay);
 }
 
+function fadeIn(elementId, delay) {
+    setTimeout(() => {
+        const element = document.getElementById(elementId);
+        element.classList.add('fadeIn');
+    }, delay);
+}
+
+
+
 typeWriter("CHAEMIN LEE.", "typing-name", 80, 0);
+fadeIn("university", 1000);
 typeWriter("PROJECTS", "typing-project", 80, 1000);
 typeWriter("Real-time Stock Sentiment Analyzer", "typing-project1-name", 30, 1000);
 typeWriter("Oftentimes being sharp about how investors feel toward a stock is as crucial as understanding a company's future value. As somewhat of a trader myself, I realized there wasn't a single platform that would summarize all the bullish/bearish sentiments from platforms like Reddit, Threads, etc. and compare their sentiment/claims with facts and historical data. Using AI, this is now possible. This project aims to update the different sentiments surrounding a particular stock in real-time, while also updating with credible news sources to rebuttal certain greedy/fearmongering statements. My ambition for this project is to ultimately create an AI-simulated LIVE debate between bullish and bearish sentiments. This will streamline the decision-making process for many investors, particularly beginner ones, who can easily monitor the debate and see who they agree with more.", "typing-project1-desc", 3, 1000);
